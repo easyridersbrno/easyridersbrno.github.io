955189f0eb952f204d85218d0f3c833e0d5b95d9
rplnt <rplnt@o0.sk>
2017-05-27 15:51:12 +0200

    log

--- mapy.js
+++ mapy.js
@@ -11,5 +11,7 @@ function DrawMapyWithLayer(container) {
     /* Add overlay; TODO: coords */
     var leftTop = SMap.Coords.fromWGS84(13, 50);
     var rightBottom = SMap.Coords.fromWGS84(13.9, 49.9);
+    console.log(leftTop);
+    console.log(rightBottom);
     vrstva.addImage("https://upload.wikimedia.org/wikipedia/commons/6/6b/Bitmap_VS_SVG.svg", leftTop, rightBottom);
 }
